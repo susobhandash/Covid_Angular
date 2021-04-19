e2783155bfa7e0a36667d79ab480b1db8d89fd1c
Susobhan Dash <susobhan.dash@gmail.com>
2021-04-19 21:09:14 +0530

    Updated styles for Dark Mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Covid Stats';
-  darkMode = false;
+  darkMode = true;
 }
--- src/common/components/pie-chart/pie-chart.component.ts
+++ src/common/components/pie-chart/pie-chart.component.ts
@@ -11,16 +11,20 @@ export class PieChartComponent implements OnInit {
   @Input('prop') prop:number;
   @Input('color') color:string;
   strokeColor = '';
+  state = '';
 
   constructor() { }
 
   ngOnInit(): void {
     if (this.color === 'active') {
       this.strokeColor = 'rgba(0, 123, 255, 1)';
+      this.state = 'Active';
     } else if (this.color === 'recovered') {
       this.strokeColor = 'rgba(75, 192, 192, 1)';
+      this.state = 'Recovered';
     } else if (this.color === 'deceased') {
       this.strokeColor = 'rgba(173, 173, 175, 1)';
+      this.state = 'Deceased';
     }
   }
 
@@ -28,6 +32,10 @@ export class PieChartComponent implements OnInit {
     // console.log(this.prop);
     let param1 = ((this.prop * 31.4)/this.total);
     return param1 +" "+ 31.4;
+  }
+
+  getTooltipValue() {
+    return this.state + ': ' + Math.round((this.prop/this.total)*100) + ' %';
   }
 
 }
